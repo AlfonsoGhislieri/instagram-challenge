339e6f325e4a63b88f82e07912c4d95c4a5d2f77
AlfonsoGhislieri <alfonso.ghis@hotmail.fr>
2022-01-22 16:29:10 +0000

    added more tests and unique email for user

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 FactoryBot.define do
   
   factory :user do
+    id {1}
     email {"test@gmail.com"}
     password {"test123"}
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,8 +15,22 @@ RSpec.describe User, type: :model do
   end
 
   it "is not valid without a sufficiently long password" do
-    user = build(:user, password: '123')
+    user = build(:user, password: 'test')
     expect(user).to_not be_valid
   end
-  
+
+  it "is not valid without a password" do
+    user = build(:user, password: nil)
+    expect(user).to_not be_valid
+  end
+
+  it "is not valid without an email" do
+    user = build(:user, email: nil)
+    expect(user).to_not be_valid
+  end
+
+  it "is not valid without a unique email" do
+    user = build(:user, email: 'test@gmail.com')
+    expect(user).to_not be_valid
+  end  
 end
