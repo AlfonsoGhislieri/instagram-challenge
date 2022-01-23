6a41bd2f519be1decc1239e8b36a583fda9baee9
AlfonsoGhislieri <alfonso.ghis@hotmail.fr>
2022-01-23 14:54:28 +0000

    added devise error  messages and feature test to show multiple posts;

--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -23,14 +23,29 @@ RSpec.feature 'Posts', type: :feature do
     find(:xpath, create_button).click
 
     expect(page).to have_content("Test message")
-    expect(find(:xpath, '//*[@id="post"]/img')['src'].split('/')[-1]).to eq("Scooby-Doo.png")  
+    expect(page).to have_css("img[src*='Scooby-Doo.png']")    
   end
 
   scenario "Submitting just image displays post" do
     find(:xpath, image_field).set("spec/images/ShaggyRogers.png")
     find(:xpath, create_button).click
 
-    expect(find(:xpath, '//*[@id="post"]/img')['src'].split('/')[-1]).to eq("ShaggyRogers.png")  
+    expect(page).to have_css("img[src*='ShaggyRogers.png']")     
+  end
+
+  scenario "/posts displays all posts" do
+    find(:xpath, message_field).set("Test message")
+    find(:xpath, image_field).set("spec/images/Scooby-Doo.png")
+    find(:xpath, create_button).click
+
+    visit "/posts/new"
+    find(:xpath, image_field).set("spec/images/ShaggyRogers.png")
+    find(:xpath, create_button).click
+
+    visit("/posts")
+    expect(page).to have_content("Test message")
+    expect(page).to have_css("img[src*='Scooby-Doo.png']")  
+    expect(page).to have_css("img[src*='ShaggyRogers.png']")   
   end
 
 end
