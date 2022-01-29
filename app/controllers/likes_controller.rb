8a3c42e949cd3c5c557b08dff012df2c124481b7
AlfonsoGhislieri <alfonso.ghis@hotmail.fr>
2022-01-29 18:51:32 +0000

    added like and unlike to front end

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -7,20 +7,19 @@ class LikesController < ApplicationController
       flash[:notice] = @like.errors.full_messages.to_sentence
     end
 
-    redirect_to @like.likeable
+    redirect_to posts_path
   end
 
   def destroy
     @like = current_user.likes.find(params[:id])
-    likeable = @like.likeable
     @like.destroy
 
-    redirect_to likeable
+    redirect_to posts_path
   end
 
   private
 
   def like_params
-    params.require(:like).permit(:likeable_id, :likeable_type)
+    params.permit(:likeable_id, :likeable_type)
   end
 end
